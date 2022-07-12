f632b17a86c62d80d541b529d0a5f6aecb7f3852
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-07-12 20:13:49 -0300

    Configuração inicial, organização das pastas e implementação dos primeiros detalhes do layout da aplicação

---
+++ clone-twitter/src/App.js
@@ -0,0 +1,12 @@
+import Index from "./ui/pages/index";
+import styles from './App.module.css'
+
+function App() {
+    return (
+        <div className={styles['app-container']}>
+            <Index />
+        </div>
+    );
+}
+
+export default App;
---
+++ clone-twitter/src/ui/pages/index.jsx
@@ -0,0 +1,8 @@
+import { useIndex } from '../../data/hooks/pages/useIndex.page';
+
+export default function Index(){
+    useIndex();
+    return(
+        <div>INDEX</div>
+    );
+}
